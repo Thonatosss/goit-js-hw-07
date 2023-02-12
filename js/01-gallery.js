9fba68e2226a3ee146189da6ec19817200dc1365
Mykola Romashenko <sven4369@gmail.com>
2023-02-12 20:50:15 +0200

    1 taSK

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,19 +1,39 @@
 import { galleryItems } from "./gallery-items.js";
+
 // Change code below this line
 
 const gallery = document.querySelector(".gallery");
 const galleryMarkup = createGalleryMarkup(galleryItems);
+let instance = ``;
 
 gallery.innerHTML = galleryMarkup;
 
-gallery.addEventListener("click", (evt) => {
+gallery.addEventListener("click", onImageClick);
+
+function onImageClick(evt) {
+  document.addEventListener("keydown", onEscClick);
   const link = evt.target.getAttribute("data-source");
+  if (evt.target.nodeName !== "IMG") {
+    return;
+  }
   evt.preventDefault();
-  const instance = basicLightbox.create(`
+  createModalWindow(link);
+}
+
+function onEscClick(evt) {
+  if (evt.key !== "Escape") {
+    return;
+  }
+  instance.close();
+  document.removeEventListener('keydown', onEscClick);
+}
+
+function createModalWindow(link) {
+  instance = basicLightbox.create(`
   <img src="${link}" width="800" height="600">
 `);
   instance.show();
-});
+}
 
 function createGalleryMarkup(items) {
   return items
