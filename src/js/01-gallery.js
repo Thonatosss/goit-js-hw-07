98eecac0ae003b7c4fe555ce25c04173d1d94e59
Mykola Romashenko <sven4369@gmail.com>
2023-02-12 09:47:22 +0200

    i`m trying...

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,32 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+const galleryMarkup = createGalleryMarkup(galleryItems);
+
+gallery.innerHTML = galleryMarkup;
+
+// gallery.addEventListener("click", (evt) => {
+//   const link = evt.target.getAttribute("data-source");
+//   evt.preventDefault();
+
+  
+// });
+gallery.addEventListener('click', () => {
+    const instance = basicLightbox.create(`
+    <img src="https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272_1280.jpg" width="800" height="600">
+`);
+  instance.show();
+});
+function createGalleryMarkup(items) {
+  return items
+    .map(
+      (item) =>
+        `<div class = "gallery__item">
+        <a class = "gallery__link" href = "https://cdn.pixabay.com/photo/2019/05/14/16/43/himilayan-blue-poppy-4202825_1280.jpg">
+        <img class = "gallery__image" src = "${item.preview}" data-source = "${item.original}" alt = "${item.description}" />
+        </a>
+        </div>`
+    )
+    .join();
+}
