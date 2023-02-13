3e334fbe7a813fd1888905bd0c1073e1097f151a
Mykola Romashenko <sven4369@gmail.com>
2023-02-13 08:49:17 +0200

    1

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -4,13 +4,14 @@ const galleryMarkup = createGalleryMarkup(galleryItems);
 const galleryList = document.querySelector(".gallery");
 
 galleryList.innerHTML = galleryMarkup;
-const lightbox = new SimpleLightbox(".gallery a", {
+new SimpleLightbox(".gallery a", {
      overlay: true,
      captions: true,
      captionSelector: 'img',
      captionsData: 'alt',
      captionPosition: 'bottom',
      captionDelay: 250,
+     scrollZoom: false,
     });
 
 function createGalleryMarkup(items) {
