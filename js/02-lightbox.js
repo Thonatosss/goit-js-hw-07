2d6ce36e2b66117005c79ca97ded70f2d43d442a
Mykola Romashenko <sven4369@gmail.com>
2023-02-13 08:46:04 +0200

    lightbox

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,8 +1,27 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const galleryMarkup = createGalleryMarkup(galleryItems);
+const galleryList = document.querySelector(".gallery");
 
+galleryList.innerHTML = galleryMarkup;
+const lightbox = new SimpleLightbox(".gallery a", {
+     overlay: true,
+     captions: true,
+     captionSelector: 'img',
+     captionsData: 'alt',
+     captionPosition: 'bottom',
+     captionDelay: 250,
+    });
 
-console.log(galleryItems);
-
-
-
+function createGalleryMarkup(items) {
+  return items
+    .map(
+      (item) =>
+        `
+          <a class="gallery__item" href=${item.original}>
+          <img class="gallery__image" src=${item.preview} alt=${item.description} />
+        </a>
+        `
+    )
+    .join("");
+}
